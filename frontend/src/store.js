b2d0f103e2c9ca62e362b317fbd1f7d151f6af89
blurk <55154720+blurk@users.noreply.github.com>
2021-01-10 22:24:24 +0700

    user login reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,19 +6,26 @@ import {
 	productDetailReducer,
 	productListReducer,
 } from './reducers/productReducers';
+import { userLoginReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetail: productDetailReducer,
 	cart: cartReducer,
+	userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
 	? JSON.parse(localStorage.getItem('cartItems'))
 	: [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+	? JSON.parse(localStorage.getItem('userInfo'))
+	: NULL;
+
 const initialState = {
 	cart: { cartItems: cartItemsFromStorage },
+	userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
