5926381c0e508e7ab1e7df1760852702a3fd00c7
blurk <nsinh6745@gmail.com>
2022-07-09 13:53:32 +0700

    update theme

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 export default function SearchBox({ history }) {
-	const [keyword, setKeyword] = useState('');
+	const [keyword, setKeyword] = useState('')
 	const submitHandler = (e) => {
-		e.preventDefault();
+		e.preventDefault()
 		if (keyword.trim()) {
-			history.push(`/search/${keyword}`);
+			history.push(`/search/${keyword}`)
 		} else {
-			history.push('/');
+			history.push('/')
 		}
-	};
+	}
 	return (
 		<Form onSubmit={submitHandler} inline>
 			<Form.Control
@@ -20,9 +20,9 @@ export default function SearchBox({ history }) {
 				placeholder='Tìm kiếm sản phẩm...'
 				className='mr-sm-2 ml-sm-5'
 			/>
-			<Button type='submit' variant='outline-success' className='p-2'>
+			<Button type='submit' className='p-2'>
 				Tìm kiếm
 			</Button>
 		</Form>
-	);
+	)
 }
